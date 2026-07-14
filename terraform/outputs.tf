c13ea7dccf730e25be89ac4c877c9aa4dafe5187
ashyT-Cloud <ashytech.innovate@gmail.com>
2026-07-14 06:32:08 +0000

    feat(ci): automate S3 deployament and CloudFront invalidation

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,13 @@ output "bucket_arn" {
   value = aws_s3_bucket.website.arn
 
 }
+
+output "cloudfron_domain_name" {
+  description = "CloudFront distribution domain name"
+  value       = aws_cloudfront_distribution.website.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  description = "Cloudfron distribution ID"
+  value       = aws_cloudfront_distribution.website.id
+}
